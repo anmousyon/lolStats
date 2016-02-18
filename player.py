6d83759396990b8a75b1f6020b2d643974b92802
Jared Mosley <anmousyon@gmail.com>
2016-02-18 15:39:53 -0600

    added player and team stat vars

---
+++ player.py
@@ -0,0 +1,37 @@
+class player:
+	#gold and gold usage
+	gold;
+	goldPM;
+	wards;
+	wardsPM;
+	exp;
+	expPM;
+	cs;
+	csPM;
+	monst;
+	monstPM;
+
+	#combat
+	champDmg;
+	ChampDmgPM;
+	towerDmg;
+	towerDmgPM;
+	takenDmg;
+	takenDmgPM;
+	healing;
+	healingPM;
+
+	#kills/deaths
+	killPart;
+	kills;
+	killsPM;
+	deaths;
+	deathsPM;
+	assists;
+	assistsPM;
+	kda;
+
+	#metadata
+	minPlayed;
+	pos;
+	team;
---
+++ team.py
@@ -0,0 +1,37 @@
+class team:
+	
+	#game results
+	win;
+	loss;
+	wlRatio;
+	gameLength;
+	
+	#Kills
+	firstBloodTime;
+	firstBlood;
+	kills;
+
+	#Deaths
+	firstDeathTime;
+	firstDeath;
+	deaths;
+	
+	#Tower
+	firstTowerTime;
+	firstTower;
+	towers;
+
+	#Dragon
+	firstDragonTime;
+	firstDragon;
+	dragons;
+
+	#Baron
+	firstBaronTime;
+	firstBaron;
+	barons;
+
+	#Scuttle
+	firstScuttleTime;
+	firstScuttle;
+	scuttles;
